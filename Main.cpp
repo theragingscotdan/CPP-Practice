2b3c138998d7e95f66d39fde229c5a54cc7b1638
theragingscotdan <43002238+theragingscotdan@users.noreply.github.com>
2018-09-05 14:45:48 +0100

    added Hello World
    so this was it working

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,13 @@
 // Included Libraries
 #include <stdlib.h>  // standard library
+#include <iostream>  // input and output library
 
 
 // entry point for our program
 // every C++ program needs a main() function
 int main()
 {
+	std::cout << "Hello world for the 20th time " << std::endl;
 	// this system function will wait until we press a button before continuing
 	system("PAUSE");
 
