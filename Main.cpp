62b4a407ca68aa0f74617f71add1c574b2f8a739
theragingscotdan <43002238+theragingscotdan@users.noreply.github.com>
2018-09-05 14:38:50 +0100

    added lines (see below)
    Added system pause and library include

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,15 @@
+// Included Libraries
+#include <stdlib.h>  // standard library
+
 
 // entry point for our program
 // every C++ program needs a main() function
 int main()
 {
+	// this system function will wait until we press a button before continuing
+	system("PAUSE");
+
+
 	// tell the operating system we are done
 	// and there was no error
 	return 0;
